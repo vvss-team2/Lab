0bd18ea71e98a5035684afe9af19783e3f7a93a4
Pascu-Victor <me@vpascu.ro>
2026-03-31 16:36:15 +0300

    lab3 structure

---
+++ lab3/src/main/java/com/starfruit/App.java
@@ -0,0 +1,12 @@
+package com.starfruit;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.printLn("Hello world!");
+    }
+}
