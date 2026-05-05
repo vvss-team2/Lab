74f9db06eed3b72396f7eb5e2791760dd76d3f87
AGreatDude <stefan.oana@stud.ubbcluj.ro>
2026-05-05 17:19:08 +0300

    Created the take home for lab 4

---
+++ lab5-takehome/src/test/java/org/example/features/search/package-info.java
@@ -0,0 +1,11 @@
+
+@Narrative(
+        title = "Search for definitions",
+        text = {"In order to understand a word that I don't know",
+                "As a user",
+                "I want to be able to look up the meaning of the word"},
+        cardNumber = "#123"
+)
+package org.example.features.search;
+
+import net.serenitybdd.annotations.Narrative;
